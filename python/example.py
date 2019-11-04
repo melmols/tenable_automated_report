0d922549fd1742c490b89526e285a07eeed25355
melmols <54809973+melmols@users.noreply.github.com>
2019-11-04 15:09:48 +0000

    still errors with the plugin id in get asset detls

--- python/example.py
+++ python/example.py
@@ -1,19 +1,23 @@
-#example usage of my_tenable.py
+from pytenable import my_tenable
 
 
-from my_tenable import my_tenable
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     
     #variables
-    all_scans = {}
-    list_of_assets = []
-    
-    
-    #---------------------------------------------------------#    
-    
-    #variable definition
-    
-    all_scans = get_scan_list()
-    list_of_assets = get_assets(all_scans, 'The_Scan_We_Want')
+
+    access_key = '3d53008b63d89b20eec0b03bb1b2b10683c1f2b94ffb08d9a07af0d4a448f2d0'
+    secret_key = 'fdd62df2bd06a38fb7421954675cc6b99a60029d95882af91731764a22a2d221'
+
+    tio = my_tenable.authenticate_tenable(access_key,secret_key)
+
+    #get the list of ids
+    list_of_ids = []
+    list_of_ids = my_tenable.list_assets_with_vulns()
+    asset_info = {}
+
+    #iterate through asset_ids and get their asset info
+    for asset_id in list_of_ids:
+        asset_info.update(my_tenable.get_asset_details(asset_id))
+
+        print(asset_info)
+        break
