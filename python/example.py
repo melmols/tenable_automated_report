b88f31a2c299bc7031daa2e1c3d184bd51db6afb
melmols <54809973+melmols@users.noreply.github.com>
2019-11-13 10:11:42 +0000

    Update example.py

--- python/example.py
+++ python/example.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     
     #variables
 
-    access_key = '3d53008b63d89b20eec0b03bb1b2b10683c1f2b94ffb08d9a07af0d4a448f2d0'
-    secret_key = 'fdd62df2bd06a38fb7421954675cc6b99a60029d95882af91731764a22a2d221'
+    access_key = ''
+    secret_key = ''
 
     tio = my_tenable.authenticate_tenable(access_key,secret_key)
 
